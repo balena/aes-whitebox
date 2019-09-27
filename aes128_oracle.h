5b45b9912eaf921f61ae65f7078dfda1a28d8e91
Guilherme Balena Versiani <guibv@nymgo.com>
2019-09-27 11:26:08 -0300

    Adding the LICENSE.

--- aes128_oracle.h
+++ aes128_oracle.h
@@ -1,3 +1,7 @@
+// Copyright 2019 AES-128 WBC Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 #ifndef AES128_ORACLE_H_
 #define AES128_ORACLE_H_
 
